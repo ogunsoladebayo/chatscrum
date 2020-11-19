b5d883ca481a23587fd8db88de5209310fbf03af
ogunsoladebayo <ogunsoladebayo@gmail.com>
2020-11-19 03:34:48 +0100

    video 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-	declarations: [AppComponent, SignupComponent, LoginComponent],
+	declarations: [AppComponent, SignupComponent, LoginComponent, HomeComponent],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
